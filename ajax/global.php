ecff01684fa6869d2ffb03dc83ca25d73b4fa933
PaxAuz15 <luisauz.geek@gmail.com>
2019-12-08 23:28:43 -0500

    add: conections database server
    note: doesn't exists testing about conections

---
+++ ajax/global.php
@@ -0,0 +1,13 @@
+<?php
+//ip de la pc servidor de base de datos
+define("DB_HOST","localhost");
+
+//nombre de la base de datos
+define("DB_NAME","dbSistema");
+
+//usuario de la base de datos
+define("DB_USERNAME", "root-paxauz");
+
+//contrasena del usuario de la base de datos
+define("DB_PASSWORD", "karitu15");
+?>
